518ce9388d856371dc284b7a5b6bc5af74595325
hanibengozi <65550858+hanibengozi@users.noreply.github.com>
2022-04-06 12:20:36 +0300

    update profile img and cv link

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class AboutComponent implements OnInit {
 
   constructor() { }
-  title: string = "Hello, i am Hana ";
+  title: string = "Hello, I am Hana ";
   paragraph: string = "I am a Front End / Fullstack Developer";
 
   ngOnInit() {}
