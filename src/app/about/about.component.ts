d47552ae5bb6b56472fd28393af74823257315c4
hanibengozi <65550858+hanibengozi@users.noreply.github.com>
2022-04-06 17:26:56 +0300

    ""

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -9,7 +9,7 @@ export class AboutComponent implements OnInit {
 
   constructor() { }
   title: string = "Hello, I am Hana ";
-  paragraph: string = "I am a Front End / Fullstack Developer";
+  paragraph: string = "I am a Fullstack Developer";
 
   ngOnInit() {}
 
